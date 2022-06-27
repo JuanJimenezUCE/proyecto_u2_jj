93f47fbb08f71cb2c0be076fa9fd6fe22791bdbd
Juan Jimenez <jijimenez@uce.edu.ec>
2022-06-27 13:13:32 -0500

    Tarea 12

--- src/main/java/com/uce/edu/demo/ProyectoU2EcApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2EcApplication.java
@@ -1,5 +1,6 @@
 package com.uce.edu.demo;
 
+import org.jboss.logging.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoU2EcApplication implements CommandLineRunner{
 
+	private static Logger LOG =Logger.getLogger(ProyectoU2EcApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2EcApplication.class, args);
 	}
@@ -14,7 +16,8 @@ public class ProyectoU2EcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("hola mundo");
+	
+		LOG.info("Hola mundo");
 	}
 
 }
