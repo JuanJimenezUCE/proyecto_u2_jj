c4a9f15a513ce61eb98a25b2e82f016621556c4e
Juan Jimenez <jijimenez@uce.edu.ec>
2022-06-27 20:12:53 -0500

    Taller 15: Ejercicio con jdbc

--- src/main/java/com/uce/edu/demo/ProyectoU2EcApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2EcApplication.java
@@ -1,14 +1,22 @@
 package com.uce.edu.demo;
 
-import org.jboss.logging.Logger;
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.demo.service.IPersonaJdbcService;
+import com.uce.edu.demo.to.Persona;
+
 @SpringBootApplication
 public class ProyectoU2EcApplication implements CommandLineRunner{
-
-	private static Logger LOG =Logger.getLogger(ProyectoU2EcApplication.class);
+	
+	private static final Logger LOG= LoggerFactory.getLogger(ProyectoU2EcApplication.class);
+	@Autowired
+	private IPersonaJdbcService iPersonaJdbcService;
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2EcApplication.class, args);
 	}
@@ -17,7 +25,26 @@ public class ProyectoU2EcApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 	
-		LOG.info("Hola mundo");
+		Persona persona = new Persona();
+		persona.setId(4);
+		persona.setNombre("Juan");
+		persona.setApellido("Jimenez");
+		
+		//Insertar
+		//this.iPersonaJdbcService.guardar(persona);
+		Persona per1 = new Persona();
+		per1.setId(1);
+		per1.setNombre("A");
+		per1.setApellido("B");
+		
+		//Actualizar
+		//this.iPersonaJdbcService.actualizar(per1);
+		
+		//Eliminar
+		//this.iPersonaJdbcService.eliminar(2);
+		
+        //Buscar
+	    LOG.info(this.iPersonaJdbcService.buscar(3).toString());
 	}
 
 }
