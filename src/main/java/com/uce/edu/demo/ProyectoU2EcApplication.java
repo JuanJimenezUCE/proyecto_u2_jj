c2c704eba74e59eecf7bd1c3c7477c75b4cf2b7f
Juan Jimenez <jijimenez@uce.edu.ec>
2022-07-04 20:12:59 -0500

    Taller 17

--- src/main/java/com/uce/edu/demo/ProyectoU2EcApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2EcApplication.java
@@ -7,16 +7,24 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.estudiante.modelo.Estudiante;
 import com.uce.edu.demo.estudiante.service.IEstudianteJpaService;
+import com.uce.edu.demo.repository.modelo.Persona;
+import com.uce.edu.demo.service.IPersonaJdbcService;
+import com.uce.edu.demo.service.IPersonaJpaService;
 
 @SpringBootApplication
 public class ProyectoU2EcApplication implements CommandLineRunner{
 	
 	private static final Logger LOG= LoggerFactory.getLogger(ProyectoU2EcApplication.class);
-	
+	//@Autowired
+	//private IPersonaJdbcService iPersonaJdbcService;
+
 	@Autowired
-	private IEstudianteJpaService iEstudianteJpaService;
+	private IPersonaJpaService iPersonaJpaService;
+
+	
+	//@Autowired
+	//private IEstudianteJpaService iEstudianteJpaService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2EcApplication.class, args);
@@ -25,29 +33,22 @@ public class ProyectoU2EcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		
-		LOG.info("Datos con JPA: "+this.iEstudianteJpaService.buscarPorId(4));
-		
-		Estudiante e= new Estudiante();
-		e.setId(2);
-		e.setNombre("Juan");
-		e.setApellido("Valasquez");
-		e.setEdad("26");
-		e.setGenero("M");
-		//Guardar
-		this.iEstudianteJpaService.guardar(e);
-		Estudiante e1= new Estudiante();
-		e1.setId(1);
-		e1.setNombre("Juan");
-		e1.setApellido("Jimenez");
-		e1.setEdad("25");
-		e1.setGenero("M");
-		//Actualizacion
-	    this.iEstudianteJpaService.actualizar(e);
-	    
-	    //Eliminar
-	    this.iEstudianteJpaService.eliminar(4);
-	
+
+		//LOG.info("Datos con JPA: "+this.iPersonaJpaService.buscarPorId(4));
+	    Persona p = new Persona();
+	    //p.setId(9);
+	    p.setNombre("Pepito");
+	    p.setApellido("Cayambe");
+	    //Guardar
+		this.iPersonaJpaService.guardar(p);
+	    Persona p1 = new Persona();
+	    p1.setId(4);
+	    p1.setNombre("AndreaA");
+	    p1.setApellido("SolisA");
+	    //Actualizacion
+		//this.iPersonaJpaService.actualizar(p1);
+
+		//this.iPersonaJpaService.eliminar(7);
 	}
 
 }
